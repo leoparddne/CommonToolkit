34c0779276bab94271542c2a5a8c30bd288827b4
ives <renhanlinbsl@163.com>
2024-07-09 13:34:12 +0800

    add helper

---
+++ CommonToolkit/Common.Toolkit/Helper/JwtHelper.cs
@@ -0,0 +1,44 @@
+﻿using Common.Toolkit.Model.JWT;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Common.Toolkit.Helper
+{
+    public static class JwtHelper
+    {
+        /// <summary>
+        /// 生成token
+        /// </summary>
+        /// <param name="key">加密使用的key</param>
+        /// <param name="model"></param>
+        /// <param name="expires">过期时间，默认1小时(无用，当前使用redis做相关限制)</param>
+        /// <returns></returns>
+        public static string GenerateToken(JwtSignModel model)
+        {
+            //创建claim
+            Claim[] authClaims = new[] {
+                new Claim(ClaimTypes.Name,model.Name ),
+                new Claim(ClaimTypes.UserData, JsonConvert.SerializeObject(model.Data) ),
+                new Claim(JwtRegisteredClaimNames.Sub,model.Sub),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+            IdentityModelEventSource.ShowPII = true;
+
+            SymmetricSecurityKey authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(model.Key));
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: model.Issuer,// "YLHPlatform",
+                    audience: model.Audience,//"YLHPlatform",
+                   expires: DateTime.Now.Add(model.ExpireHour),
+                   claims: authClaims,
+                   signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                   );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ CommonToolkit/Common.Toolkit/Model/JWT/JwtSignModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Common.Toolkit.Model.JWT
+{
+    public class JwtSignModel
+    {
+        /// <summary>
+        /// 主题
+        /// </summary>
+        public string Sub { get; set; }
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 超时时长默认一小时
+        /// </summary>
+        public TimeSpan ExpireHour { get; set; } = new TimeSpan(1, 0, 0);
+
+        /// <summary>
+        /// jwt包含的用户数据-可为空
+        /// </summary>
+        public object Data { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
