a41cb4f403221e23efe9933b2dc2f70dc604d2af
ives <ivesbao@163.com>
2025-03-12 18:47:32 +0800

    Update EnumHelper.cs

--- CommonToolkit/Common.Toolkit/Helper/EnumHelper.cs
+++ CommonToolkit/Common.Toolkit/Helper/EnumHelper.cs
@@ -55,70 +55,97 @@ namespace Common.Toolkit.Helper
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static List<EnumInfo> GetEnumDescList(this Enum enumObj)
-        {
-            Type type = enumObj.GetType();
-            var result = new List<EnumInfo>();
+        using Common.Toolkit.Extention;
+using System.ComponentModel;
+using System.Reflection;
 
+namespace Common.Toolkit.Helper
+{
+    public class EnumInfo
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public int Value { get; set; }
+    }
+    public static class EnumHelper
+    {
+
+        /// <summary>
+        /// 获取枚举信息(枚举名称、描述、值)
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetDesc(this Enum value)
+        {
+            var type = value.GetType();
             var names = Enum.GetNames(type).ToList();
 
             FieldInfo[] fields = type.GetFields();
             foreach (FieldInfo item in fields)
             {
-                if (item == null)
-                {
-                    continue;
-                }
                 if (!names.Contains(item.Name))
                 {
                     continue;
                 }
-
-                var attrbutes = item.GetCustomAttributes(typeof(DescriptionAttribute), false);
-
-                if (attrbutes.IsNullOrEmpty())
-                {
-                    break;
-                }
-
-                DescriptionAttribute? firstDescAttr = null;
-
-                foreach (var attr in attrbutes)
-                {
-                    if (attr is DescriptionAttribute descAttr)
-                    {
-                        firstDescAttr = descAttr;
-                        break;
-                    }
-                }
-
-
-                var value = item.GetValue(type);
-                if (value is not int intValue)
+                if (value.ToString() != item.Name)
                 {
                     continue;
                 }
-
-                if (firstDescAttr != null)
+                DescriptionAttribute[] EnumAttributes = (DescriptionAttribute[])item.
+            GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (EnumAttributes.Length > 0)
                 {
-                    result.Add(new EnumInfo
-                    {
-                        Name = item.Name,
-                        Desc = firstDescAttr.Description,
-                        Value = intValue
-                    });
+                    return EnumAttributes[0].Description;
+
                 }
                 else
                 {
-                    result.Add(new EnumInfo
+                    return string.Empty;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 获取枚举信息列表(枚举名称、描述、值)
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static List<EnumInfo> GetEnumDescList(Type type)
+        {
+            List<EnumInfo> result = new List<EnumInfo>();
+            List<string> nameList = Enum.GetNames(type).ToList();
+            FieldInfo[] fields = type.GetFields();
+            foreach (FieldInfo fieldInfo in fields)
+            {
+                if (nameList.Contains(fieldInfo.Name))
+                {
+                    DescriptionAttribute[] array = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), inherit: false);
+                    if (array.Length != 0)
                     {
-                        Name = item.Name,
-                        Value = intValue
-                    });
+                        result.Add(new EnumInfo
+                        {
+                            Name = fieldInfo.Name,
+                            Desc = array[0].Description,
+                            Value = (int)fieldInfo.GetValue(type)
+                        });
+                    }
+                    else
+                    {
+                        result.Add(new EnumInfo
+                        {
+                            Name = fieldInfo.Name,
+                            Value = (int)fieldInfo.GetValue(type)
+                        });
+                    }
                 }
             }
 
             return result;
         }
+    }
+}
+
     }
 }
